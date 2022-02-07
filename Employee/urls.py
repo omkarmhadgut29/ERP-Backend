239b4d6590995fad44399cba71db01ea8519d79b
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2022-02-07 23:04:39 +0530

    Add employee fuctionality: 7 feb 22

--- Employee/urls.py
+++ Employee/urls.py
@@ -8,6 +8,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('', views.index, name="index"),
     path('employees/', views.EmployeeList.as_view(), name="employees"),
+    path('api/employees/add/', views.AddEmployees.as_view(), name="add_employees"),
     
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- Employee/views.py
+++ Employee/views.py
@@ -2,7 +2,6 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -28,3 +27,26 @@ class EmployeeList(generics.ListCreateAPIView):
 
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+class AddEmployees(APIView):
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated] 
+
+    def post(self, request):
+        fields = Employee._meta.get_fields()
+        validData = True
+        for field in fields:
+            if field.name != "id" and field.name != "image":
+                if request.data[field.name] == '':
+                    validData = False
+                    break
+        if validData:
+            serializer = EmployeeSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'status': 200, 'message': 'Student added', 'data': serializer.data})
+            else:
+                return Response({'status': 400, 'message': serializer.errors})
+        return Response({'status': 400, 'message': 'Student not created'})
+
